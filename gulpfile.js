ed1c1dffd083f3a1dc6fc4fdaabc936b852dffb6
anthony.kirkham <anthony.kirkham@heathwallace.com>
2016-05-24 16:33:32 +1000

    Update to less references

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 
 require('./tasks/eslint');
 require('./tasks/webpack');
-require('./tasks/sass');
+require('./tasks/less');
 
 // gulp -e [dev | prod]
-gulp.task('default', ['eslint', 'webpack', 'sass']);
+gulp.task('default', ['eslint', 'webpack', 'less']);
