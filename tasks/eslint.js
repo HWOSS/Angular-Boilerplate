3762e9e37993e969941ae53acab4d287e5ebdd66
patrickmarabeas <patrick@marabeas.io>
2016-05-29 14:16:11 +1000

    Implement custom reporters

--- tasks/eslint.js
+++ tasks/eslint.js
@@ -2,22 +2,33 @@
 'use strict';
 
 var fs          = require('fs'),
+    yargs       = require('yargs'),
     gulp        = require('gulp'),
+    gif         = require('gulp-if'),
     eslint      = require('gulp-eslint'),
     cache       = require('gulp-cached'),
     JSONminify  = require('jsonminify'),
+
+    silent      = require('./args.js').silent,
+    silentLog   = require('./reporters/eslint.silent.js'),
+    verboseLog  = require('./reporters/eslint.verbose.js'),
 
     npmPkg      = JSON.parse(fs.readFileSync('./package.json', 'utf8')),
     config      = JSON.parse(JSONminify(fs.readFileSync('./tasks/config/eslint.config.json', 'utf8')));
 
 
 gulp.task('eslint', function() {
+
+  var argv = yargs.options(silent).argv;
+
   return gulp
     .src(npmPkg.paths.scripts.src)
     .pipe(cache('eslint'))
     .pipe(eslint(config))
-    .pipe(eslint.format())
-    .pipe(eslint.format('unix', function(output) {
+    .pipe(gif(argv.silent,
+      eslint.format(silentLog),
+      eslint.format('stylish')))
+    .pipe(eslint.format(verboseLog, function(output) {
       fs.writeFileSync('./tasks/logs/eslint.log', output);
     }));
 
--- tasks/stylelint.js
+++ tasks/stylelint.js
@@ -2,24 +2,34 @@
 'use strict';
 
 var fs          = require('fs'),
+    yargs       = require('yargs'),
     gulp        = require('gulp'),
     stylelint   = require('gulp-stylelint'),
+
+    silent      = require('./args.js').silent,
+    silentLog   = require('./reporters/stylelint.silent'),
 
     npmPkg      = JSON.parse(fs.readFileSync('./package.json')),
     rules       = JSON.parse(fs.readFileSync('./tasks/config/stylelint.config.json'));
 
 
 gulp.task('stylelint', function() {
-  // TODO: Handle differently depending on environment var?
+
+  var argv        = yargs.options(silent).argv,
+      reporters   = [{formatter: 'verbose', save: 'csslint.log'}];
+
+  if(argv.silent) {
+    reporters.push({formatter: silentLog});
+  } else {
+    reporters.push({formatter: 'verbose', console: true});
+  }
+
   return gulp.src(npmPkg.paths.styles.src)
     .pipe(stylelint({
       config: rules,
       failAfterError: false,
       reportOutputDir: 'tasks/logs',
-      reporters: [
-        {formatter: 'verbose', console: true},
-        {formatter: 'verbose', save: 'csslint.log'}
-      ]
+      reporters: reporters
     }));
 
 });
