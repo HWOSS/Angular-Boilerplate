6f864b8c480ddc458923f78f655d04b308e36f5e
patrickmarabeas <patrick@marabeas.io>
2016-05-29 15:13:21 +1000

    Make default gulp task useful

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,33 @@
 'use strict';
 
 var gulp        = require('gulp'),
+    gutil       = require('gulp-util'),
+    yargs       = require('yargs'),
     runSequence = require('run-sequence');
 
 
 require('./tasks/webpack');
+require('./tasks/stylelint');
+require('./tasks/eslint');
 require('./tasks/less');
 require('./tasks/html');
 
 
 // gulp [dev | uat | prod] [-s | --silent]
+
+gulp.task('default', [], function() {
+  var tasks = Object.keys(gulp.tasks).sort();
+
+  gutil.log(gutil.colors.yellow('The following tasks are available:'));
+
+  tasks.forEach(function(t) {
+    if(t === 'default') {
+      return;
+    }
+    gutil.log(gutil.colors.yellow('• gulp ' + t));
+  });
+
+});
 
 gulp.task('dev', [], function() {
   runSequence('clean', ['eslint', 'stylelint', 'webpack:dev', 'less:dev', 'html:dev']);
